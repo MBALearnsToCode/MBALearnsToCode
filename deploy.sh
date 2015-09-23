35c3ce61408b40965e6f7a6ffa5dd7b31c978600
Vinh Luong <vinh@uptake.io>
2015-09-23 16:15:39 -0500

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 mkdocs gh-deploy --clean
 aws s3 sync ./site s3://mbalearnstocode.me --region us-west-1 --delete
-# aws s3 sync ./site s3://mbalearnstocode.com --region us-west-1 --delete
+aws s3 sync ./site s3://mbalearnstocode.com --region us-west-1 --delete
