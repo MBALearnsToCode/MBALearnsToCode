4a5b4cf6f63493d75955179437a62f0e529db27a
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-09-23 00:06:50 -0500

    update .GitIgnore

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mkdocs gh-deploy --clean
+aws s3 sync ./site s3://mbalearnstocode.me --region us-west-1 --delete
+aws s3 sync ./site s3://mbalearnstocode.com --region us-west-1 --delete
